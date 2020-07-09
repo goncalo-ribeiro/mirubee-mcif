6e0e0e22bf857ad542ddbdbb29fe8fbd825f2f09
Gonçalo Graça Ribeiro <gogasman@gmail.com>
2020-07-09 02:48:33 +0100

    minimal sqrl version

--- resources/js/components/mfa/setup/sqrl.vue
+++ resources/js/components/mfa/setup/sqrl.vue
@@ -15,9 +15,15 @@
                                 <div class=" col-md-3">
                                 </div>
                                 <div class=" col-md-6">
-                                    <a class="mx-auto" id="sqrl" :href="nonce.url_login_sqrl" v-on:click="sqrlLinkClick();return true;" tabindex="-1">
+                                    <a class="mx-auto" id="sqrl" :href="nonce.url_login_sqrl" tabindex="-1">
+                                    <!--
+                                    <a class="mx-auto" id="sqrl" :href="nonce.url_login_sqrl" v-on:click="" tabindex="-1">
+                                        -->
                                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/SQRL_icon_vector_outline.svg/1200px-SQRL_icon_vector_outline.svg.png" style="margin-left:30px;margin-top:30px" class="card-img sqrl-logo" border="0" alt=" SQRL Code - Click to authenticate your SQRL identity ">
+                                        new
+                                        <!--
                                         <p style="margin-left:30px;margin-top:20px"> {!! QrCode::size(100)->generate($url_login_sqrl); !!} </p>
+                                        -->
                                     </a>
                                 </div>
                                 <div class="col-md-3">
@@ -40,29 +46,18 @@ export default {
         },
         activated(){
             this.getNonce();
-            this.gifProbe.onload = function() {          // define our load-success function
-                this.sqrlScheme = false;			// prevent retriggering of the SQRL QR code.
-                console.log('this.gifProbe.onload', this.localhostRoot + this.encodedSqrlUrl)
-                //document.location.href = this.localhostRoot + this.encodedSqrlUrl;
-            };
-            this.gifProbe.onerror = function() { // define our load-failure function
-                setTimeout( function(){ this.gifProbe.src = this.localhostRoot + Date.now() + '.gif';	}, 250 );
-            }
-            this.pollForNextPage();
         },
         data: function(){
             return {
                 nonce: null,
-                newSync: false, lastSync: false, encodedSqrlUrl: false, sqrlScheme: true,
-                gifProbe: new Image(),			// create an instance of a memory-based probe image
-                localhostRoot: 'http://localhost:25519/',	// the SQRL client listener
+
             }
         },
         props: {
             user: Object,
         },
         computed:{
-            url: function (){
+            checkStateUrl: function (){
                 return this.nonce ? this.nonce.check_state_on : null;
             }
         },
@@ -86,41 +81,7 @@ export default {
                 })
             },
 
-            sqrlLinkClick(e) {
-                console.log('sqrlLinkClick')
-                this.encodedSqrlUrl = this.nonce.encoded_url_login_sqrl
-                // if we have an encoded URL to jump to, initiate our GIF probing before jumping
-                if ( this.encodedSqrlUrl ) { this.gifProbe.onerror(); };	// trigger the initial image probe query
-            },
-
-            pollForNextPage() {
-                console.log('pollForNextPage');
-                if (document.hidden || this.nonce == null) {					// before probing for any page change, we check to 
-                    setTimeout(this.pollForNextPage, 5000);	// see whether the page is visible. If the user is 
-                    return;								// not viewing the page, check again in 5 seconds.
-                }
-
-                axios.get(this.url)
-                .then( response => {
-                    console.log(response);
-                    if(response.data.isReady == true) {
-                        console.log('response ready', response.data.nextPage);
-                        //document.location.href = response.data.nextPage;
-                    } else {
-                        console.log('response not ready');
-                        if(response.data.msg === "Time out, reload nonce!" || response.data.msg === "IP Doesnt Match!" || response.data.msg === "SQRL is disable for this user!") {
-                    
-                            Vue.toasted.show('Nut expired! Please reload this component and try again, if you want to Authenticate by SQRL', { icon : 'cancel', type: 'error'})
-
-                        } else {
-                            setTimeout(this.pollForNextPage, 5000); // next check in 5000 milliseconds 
-                        }
-                    }
-                })
-                .catch(error => {
-                    setTimeout(this.pollForNextPage, 5000); // next check in 5000 milliseconds 
-                })
-            },
+            
         }
     }
 </script>
